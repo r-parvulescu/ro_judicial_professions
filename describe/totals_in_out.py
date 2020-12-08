8b350084e14b84d8f8e8b8e29172ad2e8b0b54d2
Radu Andrei Pârvulescu <rap348@cornell.edu>
2020-12-08 18:11:23 -0500

    added function make_percent_pre_1990_table

--- describe/totals_in_out.py
+++ describe/totals_in_out.py
@@ -2,11 +2,13 @@
 Functions for calculating counts of different mobility events, across different units.
 """
 
-from operator import itemgetter
 import itertools
-from copy import deepcopy
 import natsort
+import csv
+from copy import deepcopy
+from operator import itemgetter
 from helpers import helpers
+from preprocess import sample
 
 
 # FUNCTIONS FOR COUNTS FOR WHOLE SAMPLE AS WELL AS ENTRY AND EXIT COHORTS #
@@ -172,3 +174,67 @@ def update_cohort_of_population(cohorts_dict, population_dict, entry=True, units
                 if cohorts_dict[u][year]['total_size'] != 0:
                     cohorts_dict[u][year]['chrt_prcnt_of_pop'] = helpers.percent(cohorts_dict[u][year]['total_size'],
                                                                                  yearly_unit_pop)
+
+
+# MISCELLANEOUS FUNCTIOS FOR RETRIEVING CERTAIN DISTRIBUTIONS OF INTEREST
+
+def make_percent_pre_1990_table(person_year_table, profession, out_dir, out_dir_area_samp=None, area_sample=False):
+    """
+    Make a table that shows for for every given year the percentage of people in the system who entered the system prior
+    to 1990. This is meant to show rate of decrease of socialist-era judges. Percentages are disaggregated by judicial
+    level, and dump them in a csv table.
+
+    NB: when running this metric on the sample, this function assumes that entries and departures of pre-1990 people
+        into the sample balance out, so that the sampling itself doesn't influence the before-to-after 1990 ratio.
+
+    :param person_year_table: a table of person-years, as a list of lists; assumes no header
+    :param profession: string, "judges", "prosecutors", "notaries" or "executori".
+    :param area_sample: bool, True if you want to exclusively use data from Alba, Iași, Craiova, and Ploiești
+                        appeals areas/regions; False by default
+    :param out_dir: str, directory where we want the non-area-sampled results table to live
+    :param out_dir_area_samp: str, if given it's where we want the sample-area results table to live
+    :return None
+    """
+    if area_sample:
+        appellate_areas_to_sample = ["CA1", "CA7", "CA9", "CA12"]  # I hard code this in since it changes very rarely
+        person_year_table = sample.appellate_area_sample(person_year_table, profession, appellate_areas_to_sample)
+        out_dir = out_dir_area_samp
+
+    # get handy column indexes
+    yr_col_idx = helpers.get_header(profession, 'preprocess').index('an')
+    pid_col_idx = helpers.get_header(profession, 'preprocess').index('cod persoană')
+    lvl_col_idx = helpers.get_header(profession, 'preprocess').index('nivel')
+
+    # sort table by person and year, then group table by persons
+    person_year_table = sorted(person_year_table, key=itemgetter(pid_col_idx, yr_col_idx))
+    people = [person for k, [*person] in itertools.groupby(person_year_table, key=itemgetter(pid_col_idx))]
+
+    # get the span of years
+    years = sorted(list({int(py[yr_col_idx]) for py in person_year_table}))
+
+    # initialise the nested dict holding the data, in three layers: hierarchical levels, list of counts
+    b4_1990_year_dict = {i: {yr: {"before 1990": 0, "total count": 0} for yr in years} for i in range(1, 5)}
+
+    for person in people:
+        first_career_year = int(person[0][yr_col_idx])
+        for pers_yr in person:
+            current_year = int(pers_yr[yr_col_idx])
+            current_level = int(pers_yr[lvl_col_idx])
+            b4_1990_year_dict[current_level][current_year]["total count"] += 1
+            if first_career_year <= 1990:
+                b4_1990_year_dict[current_level][current_year]["before 1990"] += 1
+
+    # calculate percent from before 1990, only for 1990 and after (before 1990s it's always 100%)
+    percs_lvl = {lvl: [] for lvl in b4_1990_year_dict}
+    for lvl in b4_1990_year_dict:
+        for yr in years:
+            if yr >= 1990:
+                percs_lvl[lvl].append(helpers.percent(b4_1990_year_dict[lvl][yr]["before 1990"],
+                                                      b4_1990_year_dict[lvl][yr]["total count"]))
+
+    # write each level timeseries to disk
+    with open(out_dir + "percent_pre_1990.csv", "w") as out_f:
+        writer = csv.writer(out_f)
+        writer.writerow(["Hierarchical Level"] + [yr for yr in years if yr >= 1990])
+        for lvl in b4_1990_year_dict:
+            writer.writerow([lvl] + percs_lvl[lvl])
